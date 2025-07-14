fa6e04617c74b27f941dc6af9cad7c29c9076e10
Jiahao Zhu <jiahao.zhu@humibot.com>
2025-07-14 14:32:02 +0800

    build the file structure

---
+++ main.py
@@ -0,0 +1,18 @@
+"""
+AIExamPlus - An AI-driven automatic exam answering system
+
+The entrance of the system
+"""
+
+import sys
+from pathlib import Path
+
+def main():
+    """
+    @TODO: Describe the whole pipeline
+    """
+
+    print("start the pipeline")
+
+if __name__ == "__main__":
+    main()
